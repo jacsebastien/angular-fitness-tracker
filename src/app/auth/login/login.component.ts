2aa9531daee07d68f0d6de11fe8da363531dd919
jacsebastien <jac.sebastien@gmail.com>
2018-03-06 18:08:38 +0100

    use reactive approach on login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loginForm = new FormGroup({
+            email: new FormControl('', {validators: [
+                Validators.required,
+                Validators.email
+            ]}),
+            password: new FormControl('', {validators: [
+                Validators.required
+            ]})
+        });
+    }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
-}
+    onSubmit() {
+        console.log(this.loginForm);
+    }
 
 }
