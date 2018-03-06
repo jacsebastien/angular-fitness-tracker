5c53bd3d07fe5b7f64fab3a4c45b7196ec13e845
jacsebastien <jac.sebastien@gmail.com>
2018-03-06 10:55:19 +0100

    configure angular material add extra packages create MaterialModule for angular material management

---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+// Custom module for managing all material modules imports
+@NgModule({
+    imports: [],
+    exports: []
+})
+export class MaterialModule {}
