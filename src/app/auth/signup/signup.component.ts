b08b61c4db9ce2857f7abdc021dd123a2823b042
jacsebastien <jac.sebastien@gmail.com>
2018-03-06 14:42:52 +0100

    add max date validator

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+    maxDate;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.maxDate = new Date();
+        // user cant select a year less than 18 years ago (he needs to be at least 18 yeras old)
+        this.maxDate.setFullYear(this.maxDate.getFullYear() - 18); // 18 years ago
+    }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
-  }
+    onSubmit(form: NgForm) {
+        console.log(form);
+    }
 
 }
