a859b2687e9e11e5a5afdf2756230fafbe43bd08
Sébastien Jacques <sebastien@X-Wing.local>
2018-03-08 11:24:53 +0100

    pass data between component and dialog

--- src/app/dialogs/stop-training-dialog/stop-training-dialog.component.ts
+++ src/app/dialogs/stop-training-dialog/stop-training-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-stop-training-dialog',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stop-training-dialog.component.css']
 })
 export class StopTrainingDialogComponent implements OnInit {
-
-  constructor() { }
+  // get programatically passed data using angular material service
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: {progress: number}) { }
 
   ngOnInit() {
   }
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -4,29 +4,44 @@ import { MatDialog } from '@angular/material';
 import { StopTrainingDialogComponent } from '../../dialogs/stop-training-dialog/stop-training-dialog.component';
 
 @Component({
-  selector: 'app-current-training',
-  templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.css']
+    selector: 'app-current-training',
+    templateUrl: './current-training.component.html',
+    styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  progress = 0;
-  timer: number;
+    progress = 0;
+    timer: number;
 
-  constructor(private dialog: MatDialog) { }
+    constructor(private dialog: MatDialog) { }
 
-  ngOnInit() {
-    this.timer = window.setInterval(() => {
-      this.progress += 1;
+    ngOnInit() {
+        // Start a timer and store it inside timer property for future use
+        this.timer = window.setInterval(() => {
+            this.progress += 1;
 
-      if(this.progress >= 100) {
+            if (this.progress >= 100) {
+                // Stop the timer
+                clearInterval(this.timer);
+            }
+        }, 200);
+    }
+
+    onStopTraining() {
+        // Stop the timer
         clearInterval(this.timer);
-      }
-    }, 200);
-  }
 
-  onStopTraining() {
-    clearInterval(this.timer);
-    this.dialog.open(StopTrainingDialogComponent);
-  }
+        // Open material dialog and store his ref inside a property
+        const dialogRef = this.dialog.open(StopTrainingDialogComponent, {
+            // Send data to the dialog
+            data: {
+                progress: this.progress
+            }
+        });
+
+        // Material dialog use observables that fires whenever it's closed
+        dialogRef.afterClosed().subscribe(result => {
+            console.log(result);
+        });
+    }
 
 }
