2b0f6c706632810c0ed9351a0ac644fbf8f7364d
Sébastien Jacques <sebastien@X-Wing.local>
2018-03-13 16:22:50 +0100

    Add auth guard to protected routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router
+} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        // allow route to be activated ONLY if isAuth is true, else redirect to login page
+        if(this.authService.isAuth()) {
+            return true;
+        } else {
+            this.router.navigate(['/login']);
+        }
+    }
+}
