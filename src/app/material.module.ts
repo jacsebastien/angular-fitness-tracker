1d932efc46d41742eddc7c13d576026815da2e7e
Sébastien Jacques <sebastien@X-Wing.local>
2018-03-14 11:26:05 +0100

    Add sorting to table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,7 +15,8 @@ import {
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule
 } from '@angular/material';
 
 // Custom module for managing all material modules imports
@@ -36,7 +37,8 @@ import {
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule
+        MatTableModule,
+        MatSortModule
     ],
     exports: [
         MatButtonModule,
@@ -54,7 +56,8 @@ import {
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule
+        MatTableModule,
+        MatSortModule
     ]
 })
 export class MaterialModule {}
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
 @Component({
-  selector: 'app-past-trainings',
-  templateUrl: './past-trainings.component.html',
-  styleUrls: ['./past-trainings.component.css']
+    selector: 'app-past-trainings',
+    templateUrl: './past-trainings.component.html',
+    styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
-  displayedColumns: string[]= ['date', 'name', 'duration', 'calories', 'state'];
-  // No need to tell that it's an arrau of Exercise, MatTableDataSource automatically assume this
-  dataSource = new MatTableDataSource<Exercise>();
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
+    // Fetch MatSort directive from the template and store it in "sort" property type of MatSort
+    @ViewChild(MatSort) sort: MatSort;
+    displayedColumns: string[] = ['date', 'name', 'duration', 'calories', 'state'];
+    // No need to tell that it's an arrau of Exercise, MatTableDataSource automatically assume this
+    dataSource = new MatTableDataSource<Exercise>();
 
-  constructor(private trainingService: TrainingService) { }
+    constructor(private trainingService: TrainingService) { }
 
-  ngOnInit() {
-    this.dataSource.data = this.trainingService.getPassedExercises();
-  }
+    ngOnInit() {
+        this.dataSource.data = this.trainingService.getPassedExercises();
+    }
+
+    ngAfterViewInit() {
+        // Sort the table in function of the property fetched from the template
+        // Only accessible after view template generation
+        this.dataSource.sort = this.sort;
+    }
 
 }
