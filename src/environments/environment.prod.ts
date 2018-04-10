7706ca543d255cbf53bc7a1e836210ab57c03fe7
Sébastien Jacques <sebastien@X-Wing.local>
2018-04-10 09:23:12 +0200

    Add firebase to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAlek_OYOXHfYpvxM8YQ-I6PUwmfSQyKqo",
+        authDomain: "ng-fitness-tracker-64b17.firebaseapp.com",
+        databaseURL: "https://ng-fitness-tracker-64b17.firebaseio.com",
+        projectId: "ng-fitness-tracker-64b17",
+        storageBucket: "ng-fitness-tracker-64b17.appspot.com",
+        messagingSenderId: "765518832588"
+    }
 };
