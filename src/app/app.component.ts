cb4d5182f320b6e07a080d83ae67a6af2521f37c
Sébastien Jacques <sebastien@X-Wing.local>
2018-04-12 14:28:09 +0200

    Manage auto redirect if not authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from './app.reducer';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(
+        private store: Store<fromRoot.State>,
+        private router: Router
+    ) {}
 
+    ngOnInit() {
+        this.store.select(fromRoot.getIsAuth)
+        .subscribe(isAuth => {
+            if(!isAuth) {
+                this.router.navigate(['']);
+            }
+        });
+    }
 }
